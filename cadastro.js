b59c507986b4c37a73a363b05b36b8f00b60af95
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-03-31 16:47:12 -0300

    Cadastro de Leads no Google Sheets

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,6 @@
 var produtoSelecionado = null
 
+// Pegando a URL da Imagem do produto selecionado
 document.querySelectorAll(".btnComprar").forEach(botao => {
     botao.addEventListener("click", e => {
         const cardItem = e.target.closest(".card-item")
@@ -12,6 +13,7 @@ document.querySelectorAll(".btnComprar").forEach(botao => {
     })
 })
 
+// Validação e envio de dados para o back-end e para o Google Sheets
 document.querySelector('#btn_form_data-caption').addEventListener('click', async(e) => {
     e.preventDefault(); // Previne o envio do formulário
 
@@ -19,6 +21,7 @@ document.querySelector('#btn_form_data-caption').addEventListener('click', async
     // Obtém os valores dos campos
     const nameInput = document.querySelector('#name').value;
     const phoneInput = document.querySelector('#phone').value;
+
 
     // Validação do nome
     const namePattern = /^[\p{L}\s]+$/u; // Apenas letras e espaços
@@ -31,16 +34,45 @@ document.querySelector('#btn_form_data-caption').addEventListener('click', async
         return; // Encerra a execução se o nome contiver números
     }
 
+
     // Validação do número de telefone
     let phone = phoneInput.replace(/\D/g, ''); // Remove tudo que não for número
     const phonePattern = /^[0-9]{2}9[0-9]{8}$/; // Expressão regular para o formato DDdnnnnnnnn
     
+    // Construção do objeto 'data' para ser enviado ao Google Sheets
+    const data = {
+        name: nameInput,
+        phone: phone,
+        productImage: produtoSelecionado || ''
+    };
+
+    // Enviar dados para o Google Sheets
+    try {
+        const sheetsResponse = await fetch('https://script.google.com/macros/s/AKfycbx2Mmq3T6No_VLGAOqpPl_6hSFqkYNgC3vJggxs0_-iSakfuUKWahxz1pSwZxQQLvZS/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+
+        const sheetsResult = await sheetsResponse.json();
+        if (sheetsResult.status !== 'success') {
+            throw new Error('Erro ao enviar dados para o Google Sheets');
+        }
+    } catch (error) {
+        console.error(error);
+        alert('Erro ao enviar dados para o Google Sheets');
+        return; // Encerra a execução se falhar ao enviar para o Google Sheets
+    }
+
+
+    // Enviar dados para o back-end
     if (!phonePattern.test(phone)) {
         alert("O número inserido está incorreto!\n Deve ser inserido apenas o DDD, o dígito 9 e o número sem traços.");
         return; // Encerra a execução se o número de telefone for inválido
     } else {
         phone = `55${phone}`; // Adiciona o código do país
-        alert(produtoSelecionado)
         try {
             const response = await fetch('https://lp-quadrosdanaturezaaoseular-back.onrender.com/send-message', {
                 method: 'POST',
@@ -61,6 +93,4 @@ document.querySelector('#btn_form_data-caption').addEventListener('click', async
             console.error(error);
         }
     }
-
-    // Se todas as validações passarem, você pode enviar os dados ou realizar outra ação
 });
